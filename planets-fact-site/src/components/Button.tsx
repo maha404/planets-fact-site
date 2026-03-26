2d1867944b2cd9c386e9155b5cb4eead5b031184
maha404 <maria_halvarsson@hotmail.se>
2026-03-26 19:27:20 +0100

    feat: add planet data and components for planets fact site
    - Added SVG assets for React and Vite.
    - Created a Button component for navigation between planet details.
    - Introduced data.json containing detailed information about planets in the Solar System.
    - Set up main entry point with React and CSS styling.
    - Configured TypeScript settings for app and node environments.
    - Established Vite configuration for the project.

---
+++ planets-fact-site/src/components/Button.tsx
@@ -0,0 +1,15 @@
+interface Props {
+  number: string;
+  text: string;
+  option: "overview" | "structure" | "geology";
+  onClick?: (option: "overview" | "structure" | "geology") => void;
+  active: boolean;
+}
+
+export default function Button({ number, text, option, onClick, active }: Props) {
+  return (
+    <button className={`btn ${active ? 'active' : ''}`} onClick={() => onClick}>
+      {number} {text}
+    </button>
+  );
+}
